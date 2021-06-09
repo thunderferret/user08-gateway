efe6b7b85f77c0c8428020295777ade0fa3ac80c
seo <vpflt992@gmail.com>
2021-06-09 22:34:43 +0900

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM amazoncorretto:11
+COPY target/*SNAPSHOT.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-Xmx400M","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar","--spring.profiles.active=docker"]
---
+++ src/main/java/com/example/gatewaynew/GatewaynewApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewaynew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaynewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaynewApplication.class, args);
+    }
+
+}
